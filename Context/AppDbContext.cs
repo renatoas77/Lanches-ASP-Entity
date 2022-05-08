333fad79555db5c949f2616ef48672fe0f92454e
renatoas77 <renato.antonio7@hotmail.com>
2022-05-08 18:07:17 -0300

    Adicionando os arquivos iniciais

---
+++ Context/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SiteLanches.Models;
+
+namespace SiteLanches.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Lanche> Lanches { get; set; }
+    }
+}
