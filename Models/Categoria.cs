30a936d46a0c8bf9e5ed0f1a1cdd778bb8b18727
renatoas77 <renato.antonio7@hotmail.com>
2022-05-09 00:57:38 -0300

    Adicionada a primeira migration

--- Models/Categoria.cs
+++ Models/Categoria.cs
@@ -1,9 +1,19 @@
-﻿namespace SiteLanches.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SiteLanches.Models
 {
     public class Categoria
     {
         public int CategoriaId { get; set; }
+
+        [Required(ErrorMessage = "Nome da categoria obrigatório")]
+        [Display(Name = "Nome")]
+        [StringLength(100, ErrorMessage = "O tamamho maximo é de 100 caracteres")]
         public string CategoriaNome { get; set; }
+
+        [StringLength(200, ErrorMessage = "O tamamho maximo é de 200 caracteres")]
+        [Required(ErrorMessage = "Descrição obrigatória")]
+        [Display(Name = "Descrição")]
         public string Descricao { get; set; }
 
         public List<Lanche> Lanches { get; set; }
--- Models/Lanche.cs
+++ Models/Lanche.cs
@@ -1,15 +1,45 @@
-﻿namespace SiteLanches.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SiteLanches.Models
 {
     public class Lanche
     {
         public int LancheId { get; set; }
+
+        [Required(ErrorMessage = "Este campo é obrigatório")]
+        [Display(Name ="Nome do lanche")]
+        [StringLength(80, MinimumLength = 10, ErrorMessage = "O nome deve ter entre 10 e 80 caracteres")]
         public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Este campo é obrigatório")]
+        [Display(Name = "Descrição do lanche")]
+        [StringLength(200, MinimumLength = 20, ErrorMessage = "A descrição deve ter entre 20 e 200 caracteres")]
         public string DescricaoCurta { get; set; }
+
+        [Required(ErrorMessage = "Este campo é obrigatório")]
+        [Display(Name = "Descrição detalhada do lanche")]
+        [StringLength(200, MinimumLength = 20, ErrorMessage = "A descrição deve ter entre 20 e 200 caracteres")]
         public string DescricaoDetalhada { get; set; }
+
+        [Required(ErrorMessage = "Informe um preço")]
+        [Display(Name = "Preço")]
+        [Column(TypeName ="decimal(10,2)")]
+        [Range(1,99.99, ErrorMessage = "O preço deve estar entre R$ 1,00 e R$ 99,00")]
         public decimal Preco { get; set; }
+
+        [Display(Name ="Caminho da imagem normal")]
+        [StringLength(200,ErrorMessage = "Tamamho maximo de 200 caracteres")]
         public string ImagemUrl { get; set; }
+
+        [Display(Name = "Caminho da imagem miniatura")]
+        [StringLength(200, ErrorMessage = "Tamamho maximo de 200 caracteres")]
         public string ImagemThumbnailUrl { get; set; }
+
+        [Display(Name ="É preferido?")]
         public bool IsLanchePreferido { get; set; }
+
+        [Display(Name = "Estoque")]
         public bool EmEstoque { get; set; }
 
         public int CategoriaId { get; set; }
