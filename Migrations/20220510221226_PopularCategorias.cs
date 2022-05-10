00514cee0a32fb547a1ab11297683dbdc20fd869
renatoas77 <renato.antonio7@hotmail.com>
2022-05-10 19:57:40 -0300

    git add .

---
+++ Migrations/20220510221226_PopularCategorias.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SiteLanches.Migrations
+{
+    public partial class PopularCategorias : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO categorias(CategoriaNome, Descricao) VALUES ('Normal','Lanche feito com ingredientes normais')");
+
+            migrationBuilder.Sql("INSERT INTO categorias(CategoriaNome, Descricao) VALUES ('Natural','Lanche feito com ingredientes naturais ou integrais')");
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE from categorias");
+        }
+    }
+}
